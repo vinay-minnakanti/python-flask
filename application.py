9508f2aa865cebe4500d50032867dd6778b59550
vinay-minnakanti <vinaybabuminnakanti@gmail.com>
2024-11-24 15:35:18 -0600

    Update application.py

--- application.py
+++ application.py
@@ -81,6 +81,5 @@ def register():
     return render_template("register.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
 
