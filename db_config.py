6fa6d87302a80a192ce4390a28b3440f167ef911
vinay-minnakanti <vinaybabuminnakanti@gmail.com>
2024-11-24 15:30:21 -0600

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,6 @@
+db_config = {
+    "host": "database-1.cze44mkecj6a.us-east-2.rds.amazonaws.com",  # Replace with your MySQL hostname
+    "user": "admin",  # Replace with your MySQL username
+    "password": "o<QUdrSIybC#rV_2]e?gcP(}B4_2",  # Replace with your MySQL password
+    "database": "login_db"   # Replace with your MySQL database name
+}
